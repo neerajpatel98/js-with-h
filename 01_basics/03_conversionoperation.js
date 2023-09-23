ea8bbf9381a4be3783644cbcc3c6c4e03cf1e7f2
neerajpatel98 <121499101+neerajpatel98@users.noreply.github.com>
2023-09-23 18:17:22 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+//let score= 33
+let score="neeraj"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber); // comes type ,number
+console.log(valueInNumber) // it will giver the output NaN means not a number ,lekin typeof karne par type number  aa raha har
+ 
+
+let isloggedIn="hitesh"
+let string=1
+let booleanisloggedin=Boolean(isloggedIn)
+let booleanstring=Boolean(string)
+console.log(typeof booleanisloggedin);
+
+console.log(booleanstring);
+// kis typer ke conversion par kaya output ayega=>
+
+/*
+1. "33"=> convert in number and output will come 33
+2. "33abc"=> convert in number and output will come NaN
+3. True=> give 1 convert in number
+4. "" => empty string give- false
+5."Neeraj"=> give true in boolean conversion
+*/
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
